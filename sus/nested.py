41c683f3642f76051a7495d3dc696ab63f2f3f78
sushant <118897403+sushant-demn@users.noreply.github.com>
2024-02-27 18:00:32 +0530

    Add files via upload

---
+++ sus/nested.py
@@ -0,0 +1,10 @@
+a = int(input("Enter number till u want to print tables: "))
+
+
+for i in range (1 , 10):
+    for j in range (1,a+1):
+        print(i*j , end=" ")
+        
+    print("\n")
+        
+    
